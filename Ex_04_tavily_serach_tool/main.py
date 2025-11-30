52f923b6b7595c9dbdafcdd8e7d9d8e8acf250ae
sivapriyab8 <sivapriyabandla8@gmail.com>
2025-11-30 18:09:10 +0530

    Langchain Agent with tavily search tool

--- Ex_04_tavily_serach_tool/main.py
+++ Ex_04_tavily_serach_tool/main.py
@@ -7,13 +7,12 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-
 llm = ChatOpenAI()
 tools=[TavilySearch()]
 agent = create_agent(model=llm,tools=tools)
 def main():
     print("Hello from langchain-course!")
-    result=agent.invoke({"messages":HumanMessage(content="search for 3 job postings with teraform, devops with >6YOE in the hyderabd on linkedin and posted in less than 1 week and list their details")})
+    result=agent.invoke({"messages":HumanMessage(content="search for 3 job postings with terraform, devops with >6YOE in the hyderabd on linkedin and posted in less than 1 week and list their details")})
     print(result)
 
 
