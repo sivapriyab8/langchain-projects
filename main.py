c06c5cbd8d4849ddb31b463dbc4fad72c9e4e13a
sivapriyab8 <sivapriyabandla8@gmail.com>
2025-11-30 12:12:21 +0530

    LangChain Agent with sattic tool data

---
+++ main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+
+tavily
+
+@tool
+def search(query:str) -> str:
+    """
+    Tool that searvhes over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+llm = ChatOpenAI()
+tools=[search]
+agent = create_agent(model=llm,tools=tools)
+def main():
+    print("Hello from langchain-course!")
+    result=agent.invoke({"messages":HumanMessage(content="Waht is the weather in Tokyo")})
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
